0aa579e7db447a466805e1d40ee3e13ff14d48b1
咔斯Sama <kasiwuhui@outlook.com>
2026-04-06 18:33:36 +0800

    更新Blog

---
+++ js/he-standard-common.js
@@ -0,0 +1,45 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://web.archive.org/web/20240506163113/https://widget.qweather.net/standard/static/css/he-standard.css?v=1.5.0'
+  var s = d.createElement('script')
+  s.src = 'https://web.archive.org/web/20240506163113/https://widget.qweather.net/standard/static/js/he-standard.js?v=1.5.0'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
+
+
+}
+/*
+     FILE ARCHIVED ON 16:31:13 May 06, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:41:11 Mar 01, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.641
+  exclusion.robots: 0.018
+  exclusion.robots.policy: 0.009
+  esindex: 0.011
+  cdx.remote: 4.659
+  LoadShardBlock: 326.86 (6)
+  PetaboxLoader3.datanode: 257.067 (8)
+  PetaboxLoader3.resolve: 213.706 (3)
+  load_resource: 248.144 (2)
+*/
